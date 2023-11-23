43c45bb5b7138373ea22def3ff609c2bbf40d62e
timoxxor <lepehintimofei2018@gmail.com>
2023-11-23 19:29:27 +0300

    tun

---
+++ src/structtun.rs
@@ -0,0 +1,19 @@
+pub struct Tun {}
+
+impl Tun {
+    pub fn init() -> tun::Configuration {
+        let mut config = tun::Configuration::default();
+        config
+            .address((10, 0, 0, 9))
+            .broadcast((192, 168, 102, 127))
+            .netmask((255, 255, 255, 0))
+            .up();
+
+        #[cfg(target_os = "linux")]
+        config.platform(|config| {
+            config.packet_information(true);
+        });
+
+        config
+    }
+}
