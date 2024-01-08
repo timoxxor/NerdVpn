c4eba0aff97068d0cce29a8f7127a2a51503f5a7
timoxxor <lepehintimofei2018@gmail.com>
2024-01-08 12:04:15 +0300

    cleared

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,6 @@
-pub mod cs {
-    pub mod client;
-}
-
 use std::error::Error;
 
-mod structtun;
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<(), Box<dyn Error>> {
     // TODO: more shit also test Tun pls
 
     Ok(())
